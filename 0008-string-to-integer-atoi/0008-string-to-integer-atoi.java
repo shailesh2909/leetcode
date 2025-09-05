9e5899de71207bf764b8a335025d3dd2b2cf267c
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2025-09-06 01:42:00 +0530

    0008-STRING-TO-INTEGER-ATOI Stats: Time: 1 ms (100%), Space: 42.6 MB (47.74%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int myAtoi(String s) {
+        if (s == null || s.isEmpty()) return 0;
+
+        int i = 0, n = s.length();
+        while (i < n && s.charAt(i) == ' ') i++;
+        if (i == n) return 0;
+
+        int sign = 1;
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+
+        long ans = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            ans = ans * 10 + (s.charAt(i) - '0');
+            if (sign == 1 && ans > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (sign == -1 && -ans < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            i++;
+        }
+
+        return (int)(ans * sign);
+    }
+}
--- README.md
+++ README.md
@@ -209,6 +209,7 @@ This is my Leetcode peoblems repo
 ## String
 |  |
 | ------- |
+| [0008-string-to-integer-atoi](https://github.com/shailesh2909/leetcode/tree/master/0008-string-to-integer-atoi) |
 | [0013-roman-to-integer](https://github.com/shailesh2909/leetcode/tree/master/0013-roman-to-integer) |
 | [0014-longest-common-prefix](https://github.com/shailesh2909/leetcode/tree/master/0014-longest-common-prefix) |
 | [0151-reverse-words-in-a-string](https://github.com/shailesh2909/leetcode/tree/master/0151-reverse-words-in-a-string) |
