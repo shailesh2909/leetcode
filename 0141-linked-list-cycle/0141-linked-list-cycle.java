ab6d15501064b814381387853f9a7220d74b9c4d
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-01-05 20:32:25 +0530

    0141-LINKED-LIST-CYCLE Stats: Time: 0 ms (100%), Space: 47 MB (14.77%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,12 +11,11 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-
         if(head == null || head.next == null)
         {
             return false;
         }
-        
+
         ListNode slow = head;
         ListNode fast = head;
 
