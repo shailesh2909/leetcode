f3bc3de75141ca4123a858526b3fa748e2f18090
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-01-10 23:54:01 +0530

    0050-POWX-N Stats: Time: 0 ms (100%), Space: 48.5 MB (12.52%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,27 @@
 class Solution {
     public double myPow(double n, int m) {
-        
-       return Math.pow(n, m);
+
+        double ans = 1.0;
+        long nn = m;
+
+        if (nn < 0) {
+            nn = -nn;
+        }
+
+        while (nn > 0) {
+            if (nn % 2 == 1) {
+                ans = ans * n;
+                nn = nn - 1;
+            } else {
+                n = n * n;
+                nn = nn / 2;
+            }
+        }
+
+        if (m < 0) {
+            ans = 1.0 / ans;
+        }
+
+        return ans;
     }
 }
