7dca4e4ee63b57e142b450a27632fb308c11e7db
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-04-02 11:11:00 +0530

    0222-COUNT-COMPLETE-TREE-NODES Stats: Time: 0 ms (100%), Space: 49.5 MB (57.08%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,15 +14,45 @@
  * }
  */
 class Solution {
+
+    public int getLeftHeight(TreeNode root)
+    {
+        int height = 0;
+        while(root != null)
+        {
+            height++;
+            root = root.left;
+        }
+
+        return height;
+    }
+
+    public int getLeftRight(TreeNode root)
+    {
+        int height = 0;
+        while(root != null)
+        {
+            height++;
+            root = root.right;
+        }
+
+        return height;
+    }
+    
     public int countNodes(TreeNode root) {
         if(root == null)
         {
             return 0;
         }
 
-        int left = countNodes(root.left);
-        int right = countNodes(root.right);
+        int left = getLeftHeight(root);
+        int right = getLeftRight(root);
 
-        return 1 + left + right;
+        if(left == right)
+        {
+            return (1 << left) - 1;
+        }
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
     }
 }
