51a1f05381dd2307718d69a94f56648b799075c8
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-05-09 11:58:28 +0530

    1013-FIBONACCI-NUMBER Stats: Time: 57 ms (9.25%), Space: 41.8 MB (84.17%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,25 +1,44 @@
 class Solution {
 
-    public int solve(int n, int[] dp)
-    {
-        if(n <= 1)
-        {
-            return n;
-        }
+    // public int solve(int n, int[] dp)
+    // {
+    //     if(n <= 1)
+    //     {
+    //         return n;
+    //     }
 
-        if(dp[n] != -1)
-        {
-            return dp[n];
-        }
+    //     if(dp[n] != -1)
+    //     {
+    //         return dp[n];
+    //     }
 
-        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-    }
+    //     return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    // }
+
+    // public int fib(int n) {
+        
+    //     int dp[] = new int[n + 1];
+    //     Arrays.fill(dp, -1);
+
+    //     return solve(n, dp);
+    // }
+
 
     public int fib(int n) {
         
-        int dp[] = new int[n + 1];
-        Arrays.fill(dp, -1);
+       if(n == 0) return 0;
+       if(n == 1) return 1;
+       
+       int prev = 1;
+       int prev2 = 0;
 
-        return solve(n, dp);
+       for(int i = 2; i < n + 1; i++)
+       {
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+       }
+
+       return prev;
     }
 }
