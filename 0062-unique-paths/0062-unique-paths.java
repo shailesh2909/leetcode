7e3f1267617cb13a0771e99255e6e5cc5d0859fa
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-05-30 23:30:54 +0530

    0062-UNIQUE-PATHS Stats: Time: 0 ms (100%), Space: 41.8 MB (95.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,6 @@
 class Solution {
 
-    public int solve(int row, int col, int m, int n, int dp[][])
+    int solve(int row, int col, int m, int n, int dp[][])
     {
         if(row == m - 1 && col == n - 1)
         {
@@ -12,32 +12,18 @@ class Solution {
             return 0;
         }
 
-        if(dp[row][col] != -1)
-        {
-            return dp[row][col];
-        }
+        if(dp[row][col] != 0) return dp[row][col];
 
         int down = solve(row + 1, col, m, n, dp);
-
         int right = solve(row, col + 1, m, n, dp);
 
         return dp[row][col] = down + right;
-        
     }
 
     public int uniquePaths(int m, int n) {
-        
+
         int dp[][] = new int[m][n];
-        
-        for(int i = 0; i < m; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                dp[i][j] = -1;
-            }
-        }
 
-        return solve(0, 0, m, n, dp);
-
+        return solve(0, 0, m, n, dp);    
     }
 }
