602193b32d4b3119564c1c1d8e02e92adf54cc80
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-07-23 13:54:15 +0530

    0229-MAJORITY-ELEMENT-II Stats: Time: 7 ms (50.68%), Space: 58.3 MB (5.43%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,64 +1,29 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int ele1 =  Integer.MIN_VALUE;
-        int ele2 = Integer.MIN_VALUE;
-        int count1 = 0;
-        int count2 = 0;
+
+        Arrays.sort(nums);
+
         List<Integer> ans = new ArrayList<>();
 
-        for(int i = 0; i < nums.length; i++)
+        int count = 1;
+        int n = nums.length;
+
+        for(int i = 1; i < n; i++)
         {
-            if(count1 == 0 && ele2 != nums[i])
+            if(nums[i] == nums[i - 1]) 
             {
-                count1 = 1;
-                ele1 = nums[i];
-            }
-            else if(count2 == 0 && ele1 != nums[i])
-            {
-                count2 = 1;
-                ele2 = nums[i];
-            }
-            else if(nums[i] == ele1)
-            {
-                count1++;
-            }
-            else if(nums[i] == ele2)
-            {
-                count2++;
+                count++;
             }
             else
             {
-                count1--;
-                count2--;
+                if(count > n / 3)
+                    ans.add(nums[i - 1]);
+                count = 1;
             }
         }
 
-        count1 = 0;
-        count2 = 0;
-
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(ele1 == nums[i])
-            {
-                count1++;
-            }
-            if(ele2 == nums[i])
-            {
-                count2++;
-            }
-        }
-
-        int mini = nums.length / 3 + 1;
-
-        if(count1 >= mini)
-        {
-            ans.add(ele1);
-        }
-
-        if(count2 >= mini)
-        {
-            ans.add(ele2);
-        }
+        if (count > n / 3)
+            ans.add(nums[n - 1]);
 
         return ans;
     }
