f2b1d53b7b548b9a8effb3b4998352710f738356
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-07-23 15:29:17 +0530

    0229-MAJORITY-ELEMENT-II Stats: Time: 2 ms (99.96%), Space: 52.5 MB (66.07%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,29 +1,66 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
 
-        Arrays.sort(nums);
-
         List<Integer> ans = new ArrayList<>();
 
-        int count = 1;
+        int count1 = 0;
+        int count2 = 0;
+        int ele1 = Integer.MIN_VALUE;
+        int ele2 = Integer.MIN_VALUE;
+
         int n = nums.length;
 
-        for(int i = 1; i < n; i++)
+        for(int i = 0; i < n; i++)
         {
-            if(nums[i] == nums[i - 1]) 
+            if(nums[i] == ele1) 
             {
-                count++;
+                count1++;
+            }
+            else if(nums[i] == ele2)
+            {
+                count2++;
+            }
+            else if(count1 == 0)
+            {
+                count1 = 1;
+                ele1 = nums[i];
+            }
+            else if(count2 == 0)
+            {
+                count2 = 1;
+                ele2 = nums[i];
             }
             else
             {
-                if(count > n / 3)
-                    ans.add(nums[i - 1]);
-                count = 1;
+                count1--;
+                count2--;
             }
         }
 
-        if (count > n / 3)
-            ans.add(nums[n - 1]);
+        count1 = 0;
+        count2 = 0;
+
+        for(int num : nums)
+        {
+            if(num == ele1)
+            {
+                count1++;
+            }
+            else if(num == ele2)
+            {
+                count2++;
+            }
+        }
+
+        if(count1 > n / 3)
+        {
+            ans.add(ele1);
+        }
+
+        if(count2 > n / 3)
+        {
+            ans.add(ele2);
+        }
 
         return ans;
     }
