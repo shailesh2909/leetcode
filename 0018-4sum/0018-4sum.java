b4c4aea6ec7267e723359e2963d3a30b551856c6
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-07-26 17:53:33 +0530

    0018-4SUM Stats: Time: 19 ms (83.54%), Space: 46.1 MB (16.9%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n = nums.length;
+        
         List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
         Arrays.sort(nums);
-
         for(int i = 0; i < n; i++)
         {
-            if(i > 0 && nums[i] == nums[i-1])
+            if(i > 0 && nums[i] == nums[i - 1])
             {
                 continue;
             }
 
             for(int j = i + 1; j < n; j++)
             {
-                if(j != (i + 1) && nums[j] == nums[j-1])
+                if(j != (i + 1) && nums[j] == nums[j - 1])
                 {
                     continue;
                 }
@@ -23,13 +23,21 @@ class Solution {
 
                 while(k < l)
                 {
-                    long sum = nums[i] + nums[j];
-                    sum += nums[k] + nums[l];
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
 
                     if(sum == target)
                     {
-                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
+
                         ans.add(temp);
+
                         k++;
                         l--;
 
@@ -38,7 +46,7 @@ class Solution {
                             k++;
                         }
 
-                        while(k < l && nums[l] == nums[l+1])
+                        while(k < l && nums[l] == nums[l + 1])
                         {
                             l--;
                         }
