5b268abe38237c210a1a133e0be2119abf32bf0b
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-07-28 12:09:18 +0530

    1372-CHECK-IF-IT-IS-A-GOOD-ARRAY Stats: Time: 3 ms (84.05%), Space: 61.9 MB (59.62%) - LeetHub

--- 1372-check-if-it-is-a-good-array/1372-check-if-it-is-a-good-array.java
+++ 1372-check-if-it-is-a-good-array/1372-check-if-it-is-a-good-array.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean isGoodArray(int[] nums) {
+        
         int g = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
+        for(int i = 1; i < nums.length; i++)
+        {
             g = gcd(g, nums[i]);
-            if (g == 1) {
+            if(g == 1)
+            {
                 return true;
             }
         }
@@ -12,9 +14,15 @@ class Solution {
         return g == 1;
     }
 
-    private int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
-    
+    int gcd(int a, int b)
+    {
+        while(b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
     }
 }
