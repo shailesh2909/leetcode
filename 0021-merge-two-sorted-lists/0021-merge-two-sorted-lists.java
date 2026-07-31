9041de27ecd8a002d94145778feeb103e3242ed7
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-07-31 11:40:25 +0530

    0021-MERGE-TWO-SORTED-LISTS Stats: Time: 0 ms (100%), Space: 44.4 MB (20.82%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,65 +10,35 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
 
-        if(list1 == null && list2 == null)
+        while(list1 != null && list2 != null)
         {
-            return null;
+            if(list1.val <= list2.val)
+            {
+                tail.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+
+            tail = tail.next;
         }
 
-        if(list1 == null)
+        if(list1 != null)
         {
-            return list2;
+            tail.next = list1;
         }
-
-        if(list2 == null)
+        else
         {
-            return list1;
+            tail.next = list2;
         }
         
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-
-        while(temp1 != null && temp2 != null)
-        {
-            arr.add(temp1.val);
-            arr.add(temp2.val);
-
-            temp1 = temp1.next;
-            temp2 = temp2.next;
-        }
-
-        while(temp1 != null)
-        {
-            arr.add(temp1.val);
-
-            temp1 = temp1.next;
-        }
-
-        while(temp2 != null)
-        {
-            arr.add(temp2.val);
-
-            temp2 = temp2.next;
-        }
-
-        Collections.sort(arr);
-
-        ListNode headTemp = new ListNode(-1);
-        ListNode temp = headTemp;
-
-        int i = 0;
-        while(i < arr.size())
-        {
-            ListNode t = new ListNode(arr.get(i));
-            System.out.println(t.val);
-            temp.next = t;
-            temp = temp.next;
-            i++;
-        }
-
-        return headTemp.next;
+        return dummy.next;
     }
 }
