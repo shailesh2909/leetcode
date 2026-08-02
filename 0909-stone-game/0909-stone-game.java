e8a82b7ac6d054373d034c6b29c5a5c6839cdfce
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-08-02 14:49:14 +0530

    0909-STONE-GAME Stats: Time: 0 ms (100%), Space: 43 MB (62.03%) - LeetHub

--- 0909-stone-game/0909-stone-game.java
+++ 0909-stone-game/0909-stone-game.java
@@ -1,25 +1,6 @@
 class Solution {
-
-    public int find(int i, int j, int dp[][], int[] piles)
-    {
-        if(i == j)
-        {
-            return piles[i];
-        }
-
-        if(dp[i][j] != 0)
-        {
-            return dp[i][j];
-        }
-
-        int take = piles[i] + find(i + 1, j, dp, piles);
-        int nonTake = piles[i] + find(i, j - 1, dp, piles);
-
-        return dp[i][j] = Math.max(take, nonTake);
-    }
     public boolean stoneGame(int[] piles) {
         
-        int dp[][] = new int[piles.length][piles.length];
-        return find(0, 0, dp, piles) > 0 ? true : false;
+        return true;
     }
 }
