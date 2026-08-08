adf1f70006cd07a5b2cfd2c9de5d9c2bbf121972
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-08-08 20:00:57 +0530

    0322-COIN-CHANGE Stats: Time: 16 ms (68.57%), Space: 47.1 MB (25.13%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,44 +1,40 @@
 class Solution {
-
-    public int solve(int ind, int target, int[] coins, int[][] dp)
-    {
-
-        if(ind == 0)
-        {
-            if(target % coins[ind] == 0)
-            {
-                return target / coins[ind];
-            }
-            else
-            {
-                return (int)1e9;
-            }
-        }
-
-        if(dp[ind][target] != 0)
-        {
-            return dp[ind][target];
-        }
-
-        int nontake = 0 + solve(ind - 1, target, coins, dp);
-
-        int take = Integer.MAX_VALUE;
-
-        if(coins[ind] <= target)
-        {
-            take = 1 + solve(ind, target - coins[ind], coins, dp);
-        }
-
-        return dp[ind][target] = Math.min(take, nontake);
-    }
-
     public int coinChange(int[] coins, int amount) {
 
         int n = coins.length;
         
         int dp[][] = new int[n][amount + 1];
 
-        int ans = solve(n - 1, amount, coins, dp);
+        for(int target = 0; target <= amount; target++)
+        {
+            if(target % coins[0] == 0)
+            {
+                dp[0][target] = target / coins[0];
+            }
+            else
+            {
+                dp[0][target] = (int) 1e9;
+            }
+        }
+
+        for(int ind = 1; ind < n; ind++)
+        {
+            for(int t = 0; t <= amount; t++)
+            {
+                int nontake = 0 + dp[ind - 1][t];
+
+                int take = Integer.MAX_VALUE;
+
+                if(coins[ind] <= t)
+                {
+                    take = 1 + dp[ind][t - coins[ind]];
+                }
+
+                dp[ind][t] = Math.min(take, nontake);
+            }
+
+        }
+        int ans = dp[n - 1][amount];
         return ans == 1e9 ? -1 : ans;
     }
 }
