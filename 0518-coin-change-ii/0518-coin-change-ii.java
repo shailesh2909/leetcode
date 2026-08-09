8736d87bf9c2959c84fe2a4eac4ea7f9d564c71b
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-08-09 16:47:22 +0530

    0518-COIN-CHANGE-II Stats: Time: 10 ms (65.64%), Space: 57.2 MB (28.32%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,45 +1,34 @@
 class Solution {
-
-    public int solve(int ind, int sum, int[] nums, int[][] dp)
-    {
-        if(ind == 0)
-        {
-            if(sum % nums[ind] == 0)
-            {
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-
-        if(dp[ind][sum] != -1)
-        {
-            return dp[ind][sum];
-        }
-
-        int nontake = solve(ind - 1, sum, nums, dp);
-        int take = 0;
-
-        if(nums[ind] <= sum)
-        {
-            take = solve(ind, sum - nums[ind], nums, dp);
-        }
-
-        return dp[ind][sum] = take + nontake;
-    }
-
     public int change(int amount, int[] coins) {
         
         int n = coins.length;
         int[][] dp = new int[n][amount + 1];
 
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i <=amount; i++)
         {
-            Arrays.fill(dp[i], -1);
+            if(i % coins[0] == 0)
+            {
+                dp[0][i] = 1;
+            }
         }
 
-        return solve(n - 1, amount, coins, dp);
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 0; j <= amount; j++)
+            {
+                int nontake = dp[i - 1][j];
+                int take = 0;
+
+                if(coins[i] <= j)
+                {
+                    take = dp[i][j - coins[i]];
+                }
+
+                dp[i][j] = take + nontake;
+            }
+        }
+
+        int ans = dp[n - 1][amount];
+        return ans == -1 ? 0 : ans;
     }
 }
