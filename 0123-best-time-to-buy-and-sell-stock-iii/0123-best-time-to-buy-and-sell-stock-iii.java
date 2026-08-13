662ad0cecbac50e16fd85574faafc81d60d8b1ff
Shailesh Suryawanshi <shaileshsuryawanshi443103@gmail.com>
2026-08-13 17:03:01 +0530

    0123-BEST-TIME-TO-BUY-AND-SELL-STOCK-III Stats: Time: 55 ms (37.46%), Space: 118 MB (28.37%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,35 +1,41 @@
 class Solution {
 
-    public int solve(int i, int buy, int[] prices, int cap, Integer [][][]dp)
-    {
-        if(i == prices.length || cap == 0)
-        {
-            return 0;
-        }
-
-        if(dp[i][buy][cap] != null)
-        {
-            return dp[i][buy][cap];
-        }
-
-        int profit = 0;
-        
-        if(buy == 1)
-        {
-            profit = Math.max(-prices[i] + solve(i + 1, 0, prices, cap, dp), solve(i + 1, 1, prices, cap, dp));
-        }
-        else
-        {
-            profit = Math.max(prices[i] + solve(i + 1, 1, prices,cap - 1, dp), solve(i + 1, 0, prices, cap, dp));
-        }
-
-        return dp[i][buy][cap] = profit;
-    }
-
     public int maxProfit(int[] prices) {
-        
-        Integer dp[][][] = new Integer[prices.length][2][3];
 
-        return solve(0, 1, prices, 2, dp);
+        int n = prices.length;
+        
+        int dp[][][] = new int[n+1][2][3];
+
+        for(int i = n - 1; i >= 0; i--)
+        {
+            for(int buy = 0; buy <= 1; buy++)
+            {
+                for(int cap = 0; cap <= 2; cap++)
+                {
+
+                    int profit = 0;
+
+                    if(buy == 1)
+                    {
+                        profit = Math.max(-prices[i] + dp[i + 1][0][cap], dp[i + 1][1][cap]);
+                    }
+                    else
+                    {
+                        if(cap > 0)
+                        {
+                            profit = Math.max(prices[i] + dp[i + 1][1][cap - 1], dp[i + 1][0][cap]);
+                        }
+                        else
+                        {
+                            profit = dp[i + 1][0][cap];
+                        }
+                    }
+
+                    dp[i][buy][cap] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][2];
     }
 }
